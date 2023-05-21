239fc7fec4460339dbb7c9e1ea6902eeabfe2674
yo30M <129662479+yo30M@users.noreply.github.com>
2023-05-21 02:11:14 +0000

    [ProfileImage]User

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get "homes/about" => 'homes#top', as: 'about'
   resources :books, only: [:index, :show, :create, :destroy]
-  resources :users, only: [:show, :index, :edit]
+  resources :users, only: [:show, :index, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
