a09437dbac3b3782000376ee463254ffc4644589
yo30M <129662479+yo30M@users.noreply.github.com>
2023-05-21 03:07:30 +0000

    [機能実装]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,7 +14,17 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
-  
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get "homes/about" => 'homes#top', as: 'about'
-  resources :books, only: [:index, :show, :create, :destroy]
+  resources :books, only: [:index, :show, :create, :destroy, :edit, :update]
   resources :users, only: [:show, :index, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
